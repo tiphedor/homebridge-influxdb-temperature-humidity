0091e0ed817ffb5a6e573e307662b00bc0050467
Martin STEFFEN <mail@tiph.io>
2019-09-22 01:03:29 +0200

    Improved pretty much everything

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 let Service, Characteristic;
 
-const Influx = require('influx')
+const InfluxDB = require('influx')
 
-const influx = new Influx.InfluxDB({
-    host: '192.168.0.201',
-    database: 'homeserver'
-})
-
-const getLastMesurement = (mesurementName, cb) => {
-    influx.query(`SELECT last(value) FROM ${mesurementName}`)
-        .then(result => cb(result[0].last))
-        .catch(console.log)
+const getLastMesurement = (influx, temperatureKeyName, humidityKeyName, cb) => {
+    influx.query(`SELECT last(value), last(value) FROM ${temperatureKeyName}, ${humidityKeyName}`)
+        .then(result => cb(null, result[1].last, result[0].last))
+        .catch(err => cb(err))
 }
 
 module.exports = homebridge => {
@@ -23,103 +18,67 @@ function HttpTempInfluxTemperatureHumidityhum(log, config) {
     this.log = log;
 
     // Configuration
-    this.url             = config["url"];
-    this.httpMethod      = config["httpMethod"] || "GET";
-    this.name            = config["name"];
-    this.manufacturer    = config["manufacturer"] || "Generic";
-    this.model           = config["model"] || "HTTP(S)";
-    this.serial          = config["serial"] || "";
-    this.humidity        = config["humidity"];
-    this.lastUpdateAt    = config["lastUpdateAt"] || null;
-    this.cacheExpiration = config["cacheExpiration"] || 60;
+    this.name = config["name"]
+    this.manufacturer = config["manufacturer"] || "Generic"
+    this.model = config["model"] || "Accesory"
+    this.serial = config["serial"] || "123-321-123"
+
+    this.influx = new InfluxDB.InfluxDB({
+        ...config['influx']
+    })
 }
 
 HttpTempInfluxTemperatureHumidityhum.prototype = {
-    getRemoteState: (service, callback) => {
-        getLastMesurement('temperature', function(temp) {
-            this.temperatureService.setCharacteristic(
-                Characteristic.CurrentTemperature,
-                temp
-            )
-            this.temperature = temp
+    // Called when HomeKit wants to read our sensor value.
+    getRemoteState: function (service, callback) {
+        getLastMesurement(this.influx, 'temperature', 'humidity', function(influxError, temp, humi) {
+            if (influxError) {
+                this.log(influxError)
+                return callback(new Error(influxError))
+            }
 
-            // this.humidityService.setCharacteristic(
-            //     Characteristic.CurrentRelativeHumidity,
-            //     res.body.humidity
-            // );
-            // this.humidity = res.body.humidity;
+            this.temperatureService.setCharacteristic(Characteristic.CurrentTemperature, temp)
+            this.humidityService.setCharacteristic(Characteristic.CurrentRelativeHumidity, humi)
 
-            callback(null, this.temperature);
+            if (service === 'temperature') {
+                return callback(null, temp)
+            } else if (service === 'humidity') {
+                return callback(null, humi);
+            }
 
+            return callback(new Error(`Unknown service ${service}`))
         }.bind(this))
-        // request(this.httpMethod, this.url)
-        //   .set("Accept", "application/json")
-        //   .use(superagentCache)
-        //   .expiration(this.cacheExpiration)
-        //   .end(function(err, res, key) {
-        //     if (err) {
-        //         this.log(`HTTP failure (${this.url})`);
-        //         callback(err);
-        //     } else {
-        //         this.log(`HTTP success (${key})`);
-
-                
-
-        //         if (this.humidity !== false) {
-                   
-        //         }
-
-        //         this.lastUpdateAt = +Date.now();
-
-        //         switch (service) {
-        //             case "temperature":
-        //                 callback(null, this.temperature);
-        //                 break;
-        //             case "humidity":
-        //                 callback(null, this.humidity);
-        //                 break;
-        //             default:
-        //                 var error = new Error("Unknown service: " + service);
-        //                 callback(error);
-        //         }
-        //     }
-        // }.bind(this));
     },
 
-    getTemperatureState: function(callback) {
-        this.getRemoteState("temperature", callback);
-    },
+    // Homekit-specific getters
+    getTemperatureState: function(callback) { this.getRemoteState("temperature", callback) },
+    getHumidityState: function(callback) { this.getRemoteState("humidity", callback) },
 
-    getHumidityState: function(callback) {
-        this.getRemoteState("humidity", callback);
-    },
 
+    // Service configuration
+    // Sets up all the capacities of the accessory, as well as the AccessoryInformation, which provides basic identification for our accessory.
     getServices: function () {
-        var services = [],
-            informationService = new Service.AccessoryInformation();
+        const informationService = new Service.AccessoryInformation()
+        this.temperatureService = new Service.TemperatureSensor(this.name)
+        this.humidityService = new Service.HumiditySensor(this.name)
 
         informationService
             .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
             .setCharacteristic(Characteristic.Model, this.model)
-            .setCharacteristic(Characteristic.SerialNumber, this.serial);
-        services.push(informationService);
-
-        this.temperatureService = new Service.TemperatureSensor(this.name);
+            .setCharacteristic(Characteristic.SerialNumber, this.serial)
+        
         this.temperatureService
             .getCharacteristic(Characteristic.CurrentTemperature)
             .setProps({ minValue: -273, maxValue: 200 })
-            .on("get", this.getTemperatureState.bind(this));
-        services.push(this.temperatureService);
+            .on("get", this.getTemperatureState.bind(this))
+        
+        this.humidityService
+            .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+            .setProps({ minValue: 0, maxValue: 100 })
+            .on("get", this.getHumidityState.bind(this))
 
-        if (this.humidity !== false) {
-            this.humidityService = new Service.HumiditySensor(this.name);
-            this.humidityService
-                .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-                .setProps({ minValue: 0, maxValue: 100 })
-                .on("get", this.getHumidityState.bind(this));
-            services.push(this.humidityService);
-        }
-
-        return services;
+        return [
+            informationService, this.temperatureService, this.humidityService
+        ]
     }
 };
